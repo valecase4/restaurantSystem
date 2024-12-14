64d697bf833a8c44e6439017fcfd738b77ba390b
valecase4 <valeriocaselli50@gmail.com>
2024-12-14 09:32:05 +0100

    Populated database with random data.

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ conn = sqlite3.connect("test.db")
 
 # Execute SQL statements and fetch resuults from SQL queries
 cur = conn.cursor()
+
+# cur.execute("DROP TABLE reservations")
+
+# Create a table for reservations
+cur.execute("CREATE TABLE IF NOT EXISTS reservations(id, booking_date, booking_time, name, phone_number, people_number)")
