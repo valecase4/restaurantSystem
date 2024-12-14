5f0075a89ff59d1b4f41f9953b4c8276991939d4
valecase4 <valeriocaselli50@gmail.com>
2024-12-14 10:14:00 +0100

    Implemented an HTML button for adding a new reservation and handled its style.

--- app.py
+++ app.py
@@ -14,13 +14,19 @@ columnDefs = [
     { 'field': 'Booking Time'},
     { 'field': 'Booking Name' },
     { 'field': 'Phone Number' },
-    { 'field': 'People Number' }
+    { 'field': 'People Number' },
 ]
 
 grid = dag.AgGrid(
     id='reservation-table',
     rowData=reservations,
     columnDefs=columnDefs,
+    defaultColDef={
+        "sortable": True,
+        "filter": True,
+        "resizable": True,
+        "flex": 1
+    },
     style={
         "height": "100%",
         "width": "100%",  
@@ -29,7 +35,28 @@ grid = dag.AgGrid(
 )
 
 app.layout = html.Div(
-    [grid],
+    [
+        grid,
+
+        # Fixed Button for adding a new reservation
+        html.Button(
+            "+",
+            id='open-modal-btn',
+            style={
+                "position": "fixed",
+                "top": "50%",
+                "right": "20px",
+                "transform": "translateY(-50%)",
+                "fontSize": "24px",
+                "padding": "15px 25px",
+                "backgroundColor": "#4CAF50",
+                "color": "white",
+                "border": "none",
+                "borderRadius": "100%",
+                "cursor": "pointer",
+            }
+        )
+    ],
     style={
         "height": "100vh",
         "width": "100%",
