5d0e79e1f3a604d313fdef79fd8a6154df6b3983
valecase4 <valeriocaselli50@gmail.com>
2024-12-14 10:00:03 +0100

    Crated a grid that shows all the reservations in an organized way.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, htmls
+from dash import Dash, html
 import dash_ag_grid as dag
 from utils import get_reservations_data
 
@@ -6,7 +6,37 @@ data = []
 
 reservations = get_reservations_data("test.db")
 
-app = Dash()
+app = Dash(__name__)
+
+columnDefs = [
+    { 'field': 'ID' },
+    { 'field': 'Booking Date' },
+    { 'field': 'Booking Time'},
+    { 'field': 'Booking Name' },
+    { 'field': 'Phone Number' },
+    { 'field': 'People Number' }
+]
+
+grid = dag.AgGrid(
+    id='reservation-table',
+    rowData=reservations,
+    columnDefs=columnDefs,
+    style={
+        "height": "100%",
+        "width": "100%",  
+    },
+    className="ag-theme-alpine-dark"
+)
+
+app.layout = html.Div(
+    [grid],
+    style={
+        "height": "100vh",
+        "width": "100%",
+        "margin": "0",
+        "padding": "0"
+    }
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
